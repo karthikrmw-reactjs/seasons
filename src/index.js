ae75078cd846cac95b70a3a0609d4008a70f050e
karthik.sriram <ksriram@addteq.com>
2019-09-10 20:50:03 -0400

    modify app to use functional ompoenets with hooks

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
 import React from 'react'
 import ReactDOM  from 'react-dom'
+
 import SeasonDisplay from './SeasonDisplay'
 import Spinner from './Spinner'
+import useLocation from './useLocation';
 
+const App = () => {
+    const [ lat , errorMessage ] = useLocation();
 
-class App extends React.Component {
-    state = { lat : null , errorMessage : '' };
-
-    // invoked only only once, good to load data which needs to done once
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position)  => { this.setState({ lat : position.coords.latitude })  },
-            (err)       => { this.setState({ errorMessage : err.message }) }
-        );
-    }
-
-    // required method
-    render(){
-        if(this.state.errorMessage === '' ){
-            if( this.state.lat === null ){
-                return ( <Spinner message = 'Please accept location request'/> );
-            }else{
-                return ( <SeasonDisplay lat={this.state.lat} /> )
-            }
+    if(errorMessage === '' ){
+        if( lat === null ){
+            return ( <Spinner message='Please accept location request'/> );
         }else{
-            return ( <div> {this.state.errorMessage} </div> );
+            return ( <SeasonDisplay lat={lat} /> )
         }
+    }else{
+        return ( <div> {errorMessage} </div> );
     }
-
 }
+
+
+// class App extends React.Component {
+//     state = { lat : null , errorMessage : '' };
+//
+//     // invoked only only once, good to load data which needs to done once
+//     componentDidMount(){
+//         window.navigator.geolocation.getCurrentPosition(
+//             (position)  => { this.setState({ lat : position.coords.latitude })  },
+//             (err)       => { this.setState({ errorMessage : err.message }) }
+//         );
+//     }
+//
+//     // required method
+//     render(){
+//         if(this.state.errorMessage === '' ){
+//             if( this.state.lat === null ){
+//                 return ( <Spinner message = 'Please accept location request'/> );
+//             }else{
+//                 return ( <SeasonDisplay lat={this.state.lat} /> )
+//             }
+//         }else{
+//             return ( <div> {this.state.errorMessage} </div> );
+//         }
+//     }
+//
+// }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
